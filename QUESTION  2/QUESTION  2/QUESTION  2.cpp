e0da953e0739c7a214b9f63c1280947fdf56b223
AhmedMahmoud129 <66893115+AhmedMahmoud129@users.noreply.github.com>
2021-03-04 17:29:48 +0200

    edit

--- QUESTION  2/QUESTION  2/QUESTION  2.cpp
+++ QUESTION  2/QUESTION  2/QUESTION  2.cpp
@@ -4,10 +4,26 @@ using namespace std;
 
 int main()
 {
+    // Method 1
     cout << "   1" << endl;
     cout << "  121" << endl;
     cout << " 12321" << endl;
     cout << "1234321" << endl;
+
+    // Method 2
+     for(int a=1; a<=4;a++){
+       for(int b=3;b>=a;b--){
+        cout<<" ";
+}
+       for(int c=1;c<a;c++){
+       cout<<c;
+}
+       for (int d=a;d>=1;d--){
+          cout<<d;
+}
+         cout<<endl;
+}
+
 
     return 0;
 }
